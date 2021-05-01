b4716adb5c6d27a8772813a1447676eff745e599
Darlan de Castro <darlandc@gmail.com>
2021-05-01 15:50:00 -0300

    adding cors

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable, HttpException } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getAnError(): string {
-    throw new HttpException("Internal Server Error", 500)
+    throw new HttpException({
+      status: 500,
+    }, 500)
   }
 }
